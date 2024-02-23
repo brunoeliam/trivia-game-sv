0f1402459146fddac325943eb639d823608453ca
Bruno Elian Mercapide <25409519+brunoeliam@users.noreply.github.com>
2024-02-23 20:53:52 -0300

    Delete src/_codux directory

--- src/_codux/boards/answer/answer-red.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import Answer from '../../../components/Answer';
-
-export default createBoard({
-    name: 'AnswerRed',
-    Board: () => <Answer text="opcion2" color={'red'} />,
-    isSnippet: true,
-});
